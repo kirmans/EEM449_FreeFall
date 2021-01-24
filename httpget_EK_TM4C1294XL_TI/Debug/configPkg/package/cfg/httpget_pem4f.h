a3c180ea192a6a984313f072e7629d3e2d0a3d16
kirmans <34458962+kirmans@users.noreply.github.com>
2021-01-25 01:08:28 +0300

    Add files via upload

---
+++ httpget_EK_TM4C1294XL_TI/Debug/configPkg/package/cfg/httpget_pem4f.h
@@ -0,0 +1,43 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-B06
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle semaphore0;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox0;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox1;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task0;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task1;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task2;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox2;
+
+#include <ti/sysbios/hal/Timer.h>
+extern const ti_sysbios_hal_Timer_Handle timer0;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox3;
+
+#include <ti/sysbios/knl/Task.h>
+extern const ti_sysbios_knl_Task_Handle task3;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
